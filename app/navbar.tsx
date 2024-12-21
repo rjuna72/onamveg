d676f2ed3b447e05d3779cb6a02f1fee33ec67f8
Rjun <rjun5689@gmail.com>
2024-12-21 16:32:21 +1000

    bug

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,29 +1,29 @@
 import React from "react";
+import Link from "next/link"; // Import Next.js Link for navigation
 
 const Navbar = () => {
   return (
     <header className="bg-yellow-200 border-b border-yellow-300 py-6">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-3xl font-extrabold font-serif text-yellow-800">
-            <a href="/">Onam.Veg</a>
-          
+          <Link href="/">Onam.Veg</Link>
         </h1>
         <nav>
           <ul className="flex space-x-6">
             <li>
-              <a href="/menu" className="text-yellow-700 hover:underline">
+              <Link href="/menu" className="text-yellow-700 hover:underline">
                 Menu
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/about" className="text-yellow-700 hover:underline">
+              <Link href="/about" className="text-yellow-700 hover:underline">
                 About
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/contact" className="text-yellow-700 hover:underline">
+              <Link href="/contact" className="text-yellow-700 hover:underline">
                 Contact
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
