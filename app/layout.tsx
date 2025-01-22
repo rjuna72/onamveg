770408a5525ee0fba39e0eda8a5f3e5c8220d5bc
Rjun <rjun5689@gmail.com>
2025-01-22 14:43:08 +1000

    added some g analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,14 +26,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3ZRB8V2X38"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3ZRB8V2X38');
+            `,
+          }}
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
-        <Navbar/>
-        
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
