6de4a490ec82731bfc7c349fcdc52e77e9f6bbdd
Rjun <rjun5689@gmail.com>
2025-01-25 17:45:01 +1000

    bugg

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare global {
     interface Window {
       gtag: (command: string, targetId: string, config?: Record<string, any>) => void;
