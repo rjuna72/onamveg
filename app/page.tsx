a4d7a3f3effadd3eb01802258c98d2ea4ae6e692
Rjun <rjun5689@gmail.com>
2025-01-27 19:26:22 +1000

    image problem

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <section className="relative w-full h-screen">
         <Image
           src="/images/leaf.png"
-          alt="Profile picture"
+          alt="Banana Leaf"
           layout="fill"
           objectFit="cover"
           objectPosition="center"
