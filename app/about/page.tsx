05738571d65ba0b5c15af870a30b41a3f2341458
Rjun <rjun5689@gmail.com>
2025-02-08 17:50:18 +1000

    added map embed to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,17 +8,25 @@ export default function Menu() {
           About Us
         </h1>
 
-        
-
-        {/* Soups Section */}
+        {/* About Section */}
         <div>
-          <h2 className="text-3xl font-bold text-sandal mb-6 border-b-2 border-gold pb-2">
-           
-          </h2>
-          <p className=" text-xl text-center">Born out of immense passion to give back to the community and interest to give back to the community, Onam.Veg was started as both a social enterprise and community venture, employing locals and sourcing sustainable food all in order to give the customer the most delicious and quality food in the area!</p>
-          
+          <h2 className="text-3xl font-bold text-sandal mb-6 border-b-2 border-gold pb-2"></h2>
+          <p className="text-xl text-center">
+            Born out of immense passion to give back to the community, Onam.Veg was started as both a social enterprise and community venture. Employing locals and sourcing sustainable food, we strive to offer the most delicious and quality food in the area!
+          </p>
+
+          <iframe
+            width="450"
+            height="250"
+            frameBorder="0"
+            className="border-0"
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed/v1/place?key=YOUR_API_KEY&q=Onam.Veg"
+            allowFullScreen>
+          </iframe>
         </div>
       </section>
     </div>
   );
 }
+
