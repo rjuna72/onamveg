b502054c66248eeff2bd8f5d16b2eaca21d14ec0
Rjun <rjun5689@gmail.com>
2025-12-22 21:42:22 +0530

    fresh test

--- app/global.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare global {
-    interface Window {
-      gtag: (command: string, targetId: string, config?: Record<string, any>) => void;
-    }
-  }
-  
-  export {};
-  
