449e955c3d8e8acbe233b313b38b2e49cfa03100
rjun72 <rjun5689@gmail.com>
2026-04-24 14:39:40 +0300

    Add complete Google Tag Manager setup with noscript

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 
 
@@ -17,6 +18,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-white text-[#1a1a1a]">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TF7PMG8R"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="beforeInteractive"
+        >
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TF7PMG8R');`}
+        </Script>
+        
         <Navbar />
         {children}
         <Footer />
